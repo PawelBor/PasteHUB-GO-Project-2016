c7330992c97c7d215e0759e4fad75aa2c864fde4
Gediminas Saparauskas <saparauskas@gmail.com>
2016-11-08 01:42:25 +0000

    Adapted to work on the tinymce editor.html

--- templates/js/header-scripts.js
+++ templates/js/header-scripts.js
@@ -1,20 +1,78 @@
+// Websocket code adapted from github.com/gorilla/websocket/examples/chat
+var conn;
+var msg;
+var doc;
+var userInputTimer;
+
+window.onload = function () {
+    doc = document.getElementById("document");
+
+    if (window["WebSocket"]) {
+        conn = new WebSocket("ws://{{$}}/ws");
+        conn.onclose = function (evt) {
+            window.alert("Connection closed.");
+        };
+        conn.onmessage = function (evt) {
+          /*
+          var doScroll = doc.scrollTop === doc.scrollHeight - doc.clientHeight;
+          if (doScroll) {
+              doc.scrollTop = doc.scrollHeight - doc.clientHeight;
+          }*/
+          
+          // Sets the HTML contents of the activeEditor editor
+          tinyMCE.activeEditor.setContent(evt.data);
+        }
+    } else {
+        window.alert("Your browser does not support WebSockets.");
+    }
+  };
+
+function timerUpdate(){
+
+// Timer adapted from: http://stackoverflow.com/questions/4220126/run-javascript-function-when-user-finishes-typing-instead-of-on-key-up
+
+clearTimeout(userInputTimer);
+
+userInputTimer = setTimeout(function() {
+  if (!conn) {
+    console.log("No ws connection");
+    return false;
+  }
+  if (!(tinyMCE.get('tiny_content').getContent())) {
+    console.log("Message is null");
+    return false;
+  }
+  conn.send(tinyMCE.get('tiny_content').getContent());
+  return false;
+  console.log("keydown in doc");
+}, 1000);
+
+}
+
 //Apapted from: https://www.tinymce.com/docs/
 tinymce.init({
-
   selector: 'textarea',
+    setup: function(ed) {
+      ed.on('keyup', function(e) {
+          //console.log('Editor contents was modified. Contents: ' + ed.getContent());
+          timerUpdate();
+      });
+  },
   statusbar: false,
   height: 850,
   width: 810,
+  max_width: 720,
   browser_spellcheck: true,
-  toolbar: 'save undo redo styleselect bold italic alignleft aligncenter pagebreak alignright bullist numlist outdent indent code forecolor backcolor fontsizeselect',
+  toolbar: 'save undo redo styleselect bold italic alignleft aligncenter alignright bullist numlist outdent indent code forecolor fontsizeselect pagebreak',
   fontsize_formats: '8pt 10pt 12pt 14pt 18pt 24pt 36pt 46pt 56pt',
   themes: "inlite",
-  plugins: "save code fullpage textcolor pagebreak",
+  plugins: "save code fullpage textcolor pagebreak ",
   save_enablewhendirty: true,
 
   save_onsavecallback: function () {
   	save_content(); 
   }
+
 
 });
 
